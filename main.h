e4e8c96356cafe411e71608f7c3ff9e7d09524f8
“vintage254” <derricknjuguna414@gmail.com>
2023-11-13 08:14:17 -0800

    trials

--- main.h
+++ main.h
@@ -14,6 +14,21 @@
 int printchar(char c);
 
 int _printf(const char *format, ...);
+/* functions for printing chars and strings */
+
+int printstr(va_list type, char buff[],
+                int flags, int width, int precision, int size);
+int printfchar(va_list type, char buff[],
+                int flag, int width, int precision, int size);
+int printfpercent(va_list type, char buff[],
+                int flag, int width, int precision, int size);
+
+int _printf(const char *format, ...);
+/*function for printing integer(d% i%)*/
+
+int p_int(va_list typeof, char b[],
+        int flag_int, int w, int precision_, int s)
+
 /*string and printf */
 int string(char *c);
 #endif
